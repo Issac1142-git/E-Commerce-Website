42099e31953fcdfe63acccd94a65b5e54fcb3694
Issac1142-git <issacimmanuvel.t@gmail.com>
2021-05-17 13:33:52 +0530

    bug fixed

--- server.js
+++ server.js
@@ -27,9 +27,7 @@ if (process.env.NODE_ENV === "production") {
 const port = process.env.PORT || 5000;
 
 app.get("/service-worker.js", (req, res) => {
-  res.sendFile(
-    path.resolve(__dirname, "..", "build", "registerServiceWorker.js")
-  );
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
 });
 
 app.post("/payment", (req, res) => {
