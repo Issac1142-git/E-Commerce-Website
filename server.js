2c5deccad0936ab6b838d307ef1e1c94b2226c06
Issac1142-git <issacimmanuvel.t@gmail.com>
2021-05-17 13:11:35 +0530

    bug fixed

--- server.js
+++ server.js
@@ -12,24 +12,25 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(compression());
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
 app.use(cors());
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 if (process.env.NODE_ENV === "production") {
+  app.use(compression());
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  app.get("/service-worker.js", (req, res) => {
-    res.sendFile(
-      path.resolve(__dirname, "..", "build", "registerServiceWorker.js")
-    );
-  });
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   });
 }
 
 const port = process.env.PORT || 5000;
+
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "..", "build", "registerServiceWorker.js")
+  );
+});
 
 app.post("/payment", (req, res) => {
   const body = {
